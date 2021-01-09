96fbb813975d301b6431c109d76c5fd6b255eb2c
ATMANE <include@gmx.fr>
2021-01-09 11:31:12 +0100

    Implementation of the 'empty' function in order to empty the history for a new search + diverses mises en formes

--- script.js
+++ script.js
@@ -46,40 +46,62 @@ $(document).ready(function () {
     //Hide form by clicking cancel button
     cancelBtn.click(function () {
         divForm.hide();
+        $("#content").show();
         divResult.hide();
         btn.show();
     });
 
     //Création of the contain DIV Result
-    let divResult = $('<div id="result"></div>');
-    $("#myBooks").after(divResult)
+    let divResult = $('<div id="result"><p id="rcherche">Résultats de la recherche<p></div>');
+    let divBox = $('<div id="box"></div>');
+    divBox.appendTo(divResult);
+
+    $("#myBooks").after(divResult);
 
     searchBtn.click(function () {
         divResult.show();
+        $("#content").hide();
         let searchTitle = $input1.val();
         let searchAuthor = $input1.val();
         if (searchTitle == '' && searchAuthor == '') {
             alert("Veuillez saisir le titre d'un livre SVP....");
-        }  else {
-            let identifiant = '';
+        } else {
+            let id = '';
             let title = '';
             let author = '';
-            let description = '';
-            let img = '';
+            let descrip = '';
+            let image = '';
+
+
+
 
             $.get("https://www.googleapis.com/books/v1/volumes?q=search+terms" + searchTitle, function (response) {
-                for (i = 0; i < response.items.length; i++) {
-                    let divCard = $("<div id='card" + i + "'></div>")
-                    authors = response.items[i].volumeInfo.authors
-                    title = response.items[i].volumeInfo.title
-                    
+                
+            
+            $(divBox).empty();
+            for (i = 0; i < response.items.length; i++) {
+                    divCard = $("<div id='container'  + i>" + "</div>");
+                    let bookmark = $('<aside id="bmark"><i class="fa fa-bookmark"></i></aside>');
+                    bookmark.appendTo(divCard);
 
-                    divCard.append(authors);
+                    title = $("<div class='center-align'><h4>Title: " + response.items[i].volumeInfo.title + "</h4>");
+                    id = $("<div class='center-align black-text'><h4>Id: " + response.items[i].id + "</h4>");
+                    author = $("<div class='center-align black-text'><h4>Author: " + response.items[i].volumeInfo.authors + "</h4>");
+                    descrip = $("<div class='center-align black-text'><h4>Descrpiption : </br> " + response.items[i].volumeInfo.description + "</h4>");
+                    image = $("<img src=" + response.items[i].volumeInfo.imageLinks.smallThumbnail + " >");
+
+
                     divCard.append(title);
-                    divCard.append(img);
-                    divCard.appendTo(divResult);
+                    divCard.append(id);
+                    divCard.append(author);
+                    divCard.append(descrip);
+                    divCard.append(image);
+                    divCard.appendTo(divBox);
                 }
-            })}
-        });
+                
+            })
+            
+        }
+    });
 
 })
